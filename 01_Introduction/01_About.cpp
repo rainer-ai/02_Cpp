8991a31fff26eab9ad8bce1e2c42b695ca584f92
RAINER.AI <u2@rainer.ai>
2023-11-22 09:16:12 +0100

    mv file

---
+++ 01_Introduction/01_About.cpp
@@ -0,0 +1,12 @@
+// Welcome to the world of "C++"!
+
+// Some C++-Characteristics:
+
+/********************************************
+* General-purpose programming languag  [*] 
+* Domain-specific programming language [ ]
+* Statically-Typed                     [*] 
+* Dynamically-Typed                    [ ]
+* Strongly Typed                       [*] 
+* Weakly typed                         [ ]
+*********************************************/
